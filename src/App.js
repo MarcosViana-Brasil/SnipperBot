ec8f2382a9036888c09be3d7695b20e0becd9eae
Marcos Viana <marcos1968@gmail.com>
2021-08-30 15:54:41 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import useWebSocket from 'react-use-websocket'
+import './App.css'
 
 function App() {
+
+  const [ticker, setTicker] = useState({})
+  const [tradingView, setTradingView] = useState({})
+
+  const { lastJsonMessage } = useWebSocket('wss://stream.binance.com:9443/stream?streams=btcusdt@ticker', {
+    onMessage: () => {
+      if (lastJsonMessage && lastJsonMessage.data) {
+        if (lastJsonMessage.stream === 'btcusdt@ticker') {
+          setTicker(lastJsonMessage.data)
+        }
+      }
+    },
+    onError: (event) => {
+
+    }
+  })
+
+  useEffect(() => {
+    const tv = new window.TradingView.widget(
+      {
+        "autosize": true,
+        "symbol": "BINANCE:BTCUSDT",
+        "interval": "60",
+        "timezone": "Etc/UTC",
+        "theme": "dark",
+        "style": "1",
+        "locale": "en",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "allow_symbol_change": true,
+        "details": true,
+        "container_id": "tradingview_318cb"
+      }
+    )
+    setTradingView(tv)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>SnipperBot 1.0</h1>
+      <div class="tradingview-widget-container">
+        <div id="tradingview_318cb"></div>
+      </div>
+      <div>
+        <div>
+          <b>Ticker 24h:</b><br />
+          Open: {ticker && ticker.o} <br />
+          High: {ticker && ticker.h} <br />
+          Low:  {ticker && ticker.l} <br />
+          Last: {ticker && ticker.c} <br />
+          Change %: {ticker && ticker.P}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
